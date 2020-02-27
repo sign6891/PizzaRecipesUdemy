33f086295ac4884cd02581afaae32d3dc3df6db9
Janessa Leyshren <jleyshre@at-c3.21-school.ru>
2020-02-27 19:14:33 +0300

    version 2

--- app/src/main/java/com/example/pizzarecipesudemy/RecipeActivity.java
+++ app/src/main/java/com/example/pizzarecipesudemy/RecipeActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class RecipeActivity extends AppCompatActivity {
@@ -13,13 +14,16 @@ public class RecipeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
 
+        ImageView imageView = findViewById(R.id.pizzaIV);
         TextView title = findViewById(R.id.titleTextView);
         TextView recipe = findViewById(R.id.recipeTextView);
 
         Intent intent = getIntent();
         if (intent != null) {
+            imageView.setImageResource(intent.getIntExtra("imageResource", 0));
             title.setText(intent.getStringExtra("title"));
             recipe.setText(intent.getStringExtra("recipe"));
+
         }
     }
 }
